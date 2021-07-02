4aa771e0bd22b158ed10b32773078d381e723a71
Pontus Sidlo <poppe9898@hotmail.com>
2021-07-02 11:52:52 +0200

    fixes for deply

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8085;
-const serverURL = 'http://localhost:' + PORT;
+const serverURL = process.env.BASE_URL + PORT;
 const mongoURL = process.env.MONGO_URL
 
 app.listen(PORT, () => console.log(`Server us running at ${serverURL}`));
